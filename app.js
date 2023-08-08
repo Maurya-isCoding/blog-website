070dd8a5b6861bf5a5a52e22136120072d4b3146
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2023-08-08 14:44:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from "express"
+import bodyParser from "body-parser"
+import ejs from "ejs"
+import {dirname} from "path"
+import  {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000; 
+
+app.set('views engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const getMessage = "This is a general message to display on the screen of the web page to get the full attention of the people that are reading this message. Thankyou for wasting your couple of hours and glad to here"
+const aboutMessage = "This is a random meassage that I want to display on the screen of the ejs file and I am glad to display it on your screen."
+const contactMessage = "To contact us join on discord insta and Linked and many more links that will be provided on the screen page."
+
+app.get("/",  (req, res)=>{
+    res.render("./home.ejs",{message: getMessage} );
+})
+app.get("/about",  (req, res)=>{
+    res.render("./about.ejs",{message: aboutMessage} );
+})
+app.get("/contact",  (req, res)=>{
+    res.render("./contact.ejs",{message: contactMessage} );
+})
+
+app.get("/compose",  (req, res)=>{
+    res.render("./compose.ejs" );
+})
+app.post("/compose",  (req, res)=>{
+    console.log(req.body.text);
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+    
+})
