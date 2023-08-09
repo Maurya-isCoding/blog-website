5b88c654a4ae9faf0110466900e13b19781526aa
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2023-08-09 09:53:01 +0530

    added basic functionaliy

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser"
 import ejs from "ejs"
 import {dirname} from "path"
 import  {fileURLToPath} from "url";
+import _ from "lodash"
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app = express();
 const port = 3000; 
 
 app.set('views engine', 'ejs');
+
+let posts = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -18,7 +22,10 @@ const aboutMessage = "This is a random meassage that I want to display on the sc
 const contactMessage = "To contact us join on discord insta and Linked and many more links that will be provided on the screen page."
 
 app.get("/",  (req, res)=>{
-    res.render("./home.ejs",{message: getMessage} );
+    res.render("./home.ejs",{
+        message: getMessage, 
+        posts: posts
+    } );
 })
 app.get("/about",  (req, res)=>{
     res.render("./about.ejs",{message: aboutMessage} );
@@ -31,8 +38,27 @@ app.get("/compose",  (req, res)=>{
     res.render("./compose.ejs" );
 })
 app.post("/compose",  (req, res)=>{
-    console.log(req.body.text);
-})
+    
+    const post ={
+        title: req.body.title,
+        message: req.body.content
+    };
+    posts.push(post);
+    res.redirect('/')
+});
+
+app.get("/posts/:postName", (req, res)=>{
+    const reqTitle = _.lowerCase(req.params.postName);
+    posts.forEach(post => {
+        const storedTitle = _.lowerCase(post.title);
+        if(storedTitle === reqTitle){
+            res.render("post.ejs",{
+                title: post.title,
+                content: post.message
+            });
+        }
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
